44643eb63db7406d3da197d9cd147081ea452503
ht38nhatphan <58109245+ht38nhatphan@users.noreply.github.com>
2020-11-06 19:53:44 +0700

    Enter

--- python translate/test.py
+++ python translate/test.py
@@ -2,7 +2,7 @@ import googletrans
 from googletrans import Translator
 # print(googletrans.LANGUAGES)
 t = Translator()
-a= t.translate("nhat sai",src = "vi",dest = "en")
+a= t.translate("nhatdung",src = "vi",dest = "en")
 
 print(a.text)
 
