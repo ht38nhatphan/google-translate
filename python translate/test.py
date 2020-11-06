5f08503fb9f61e76ba0229551ccb6cff02420dda
ht38nhatphan <58109245+ht38nhatphan@users.noreply.github.com>
2020-11-06 19:52:04 +0700

    master

--- python translate/test.py
+++ python translate/test.py
@@ -2,7 +2,7 @@ import googletrans
 from googletrans import Translator
 # print(googletrans.LANGUAGES)
 t = Translator()
-a= t.translate("dhdhd",src = "vi",dest = "en")
+a= t.translate("nhat sai",src = "vi",dest = "en")
 
 print(a.text)
 
