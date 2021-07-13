5259fedcfe424ec036c1fa79881e50067c5db0a2
nhat <58109245+ht38nhatphan@users.noreply.github.com>
2021-07-13 10:20:45 +0700

    Revert "'new'"

--- python_translate/test.py
+++
@@ -1,9 +0,0 @@
-# import googletrans
-from googletrans import Translator
-# print(googletrans.LANGUAGES)
-t = Translator()
-# a= t.translate("đẹp trai",src = "vi",dest = "en")
-a = t.translate('안녕하세요.')
-print(a.text)
-
-
